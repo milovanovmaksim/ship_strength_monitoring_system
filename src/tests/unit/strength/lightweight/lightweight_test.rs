dddb4c764a60b3318e48e068eeb0db5ee2bdcf11
milovanovmaksim <milovanov160386@gmail.com>
2023-10-18 01:09:49 +0300

    add some changes in lightweight tests

--- src/tests/unit/strength/lightweight/lightweight_test.rs
+++ src/tests/unit/strength/lightweight/lightweight_test.rs
@@ -38,22 +38,23 @@ mod tests {
         call_once();
         let lightweight = Lightweight::from_json_file("./src/tests/unit/strength/lightweight/data/empty_field.json".to_string());
         assert!(lightweight.is_err());
-        assert!(lightweight.unwrap_err().contains("missing field `lightweight"));
+        assert!(lightweight.unwrap_err().contains("missing field `lightweight`"));
     }
     #[test]
     fn test_number_spatiums() {
         call_once();
-        let ship_dimensions = ShipDimensions::new(125.0,20, 0.8);
-        let lightweight = Lightweight::new(1750.0, ship_dimensions.clone());
+        let number_spatiums = 20;
+        let ship_dimensions = ShipDimensions::new(125.03, number_spatiums, 0.8);
+        let lightweight = Lightweight::new(1750.0, ship_dimensions);
         let output = lightweight.lightweight_intensity();
-        assert_eq!(output.len(), ship_dimensions.number_spatiums() as usize);
+        assert_eq!(output.len(), number_spatiums as usize);
     }
 
     #[test]
     fn test_type_output() {
         call_once();
         let ship_dimensions = ShipDimensions::new(125.0,20, 0.7);
-        let lightweight = Lightweight::new(1750.0, ship_dimensions.clone());
+        let lightweight = Lightweight::new(1750.0, ship_dimensions);
         let output = lightweight.lightweight_intensity();
          assert_eq!(output.type_output(), TypeOutput::LightweightIntensity);
     }
@@ -61,8 +62,8 @@ mod tests {
     #[test]
     fn test_lightweight_intensity() {
         call_once();
-        let test_weight = 13575.0;
-        let ship_dimensions = ShipDimensions::new(235.0,20, 0.65);
+        let test_weight = 13575.73;
+        let ship_dimensions = ShipDimensions::new(235.03, 20, 0.65);
         let test_lightweight = Lightweight::new(test_weight, ship_dimensions);
         let output = test_lightweight.lightweight_intensity();
         let computed_weight = output.integral();
