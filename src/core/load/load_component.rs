4ff0bde20d9a3ced38807ad1714cac4429d6c887
milovanovmaksim <milovanov160386@gmail.com>
2023-10-19 23:06:27 +0300

    change conditions

--- src/core/load/load_component.rs
+++ src/core/load/load_component.rs
@@ -64,7 +64,6 @@ impl LoadComponent {
     ///
     /// Computes load component intensity for a spatium.
     pub fn intensity(&self, ship_demensions: &ShipDimensions) -> Vec<Spatium> {
-
         match self.spread(ship_demensions) {
             LoadComponentSpread::WithinOneSpatium => {
                 let max_intensity = |c_min: f64| { self.value * (0.5 + (c_min / ship_demensions.length_spatium())) / ship_demensions.length_spatium() };
@@ -76,10 +75,10 @@ impl LoadComponent {
                 let c_right = (self.longitudinal_center_gravity() - spatium_end_coordinate).abs();
 
                 // Ближе к правому шпангоуту теоретической шпации.
-                if (c_left > c_right) && (spatium_index - 1 >= 0)  {
+                if (c_left > c_right) && (spatium_index + 1 <= ship_demensions.number_spatiums() - 1) {
                     debug!("Ближе к правому шпангоуту теоретической шпации. c_right={}, c_left={}", c_right, c_left);
                     let mut spatiums = vec![Spatium::new(spatium_index, spatium_start_coordinate, spatium_end_coordinate, max_intensity(c_right), max_intensity(c_right))];
-                    let spatium_index = spatium_index - 1;
+                    let spatium_index = spatium_index + 1;
                     let spatium_start_coordinate = spatium_end_coordinate;
                     let spatium_end_coordinate = spatium_start_coordinate + ship_demensions.length_spatium();
                     let spatium = Spatium::new(spatium_index, spatium_start_coordinate, spatium_end_coordinate, min_intensity(c_right), min_intensity(c_right));
@@ -87,10 +86,10 @@ impl LoadComponent {
                     return  spatiums;
 
                 // Ближе к левому шпангоуту теоретической шпации
-                } else if (c_right > c_left ) && (spatium_index + 1 <= ship_demensions.number_spatiums() - 1){
+                } else if (c_right > c_left ) && (spatium_index - 1 >= 0) {
                     debug!("Ближе к левому шпангоуту теоретической шпации. c_right = {}, c_left = {}", c_right, c_left);
                     let mut spatiums = vec![Spatium::new(spatium_index, spatium_start_coordinate, spatium_end_coordinate, max_intensity(c_left), max_intensity(c_left))];
-                    let spatium_index = spatium_index + 1;
+                    let spatium_index = spatium_index - 1;
                     let spatium_end_coordinate = spatium_start_coordinate;
                     let spatium_start_coordinate = spatium_start_coordinate - ship_demensions.length_spatium();
                     let spatium = Spatium::new(spatium_index, spatium_start_coordinate, spatium_end_coordinate, min_intensity(c_left), min_intensity(c_left));
