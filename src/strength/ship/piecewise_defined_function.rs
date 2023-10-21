317a698644a3a589519a44c5932406ab304565d7
milovanovmaksim <milovanov160386@gmail.com>
2023-10-21 13:27:28 +0300

    add filled_zeros constructor

--- src/strength/ship/piecewise_defined_function.rs
+++ src/strength/ship/piecewise_defined_function.rs
@@ -1,3 +1,4 @@
+
 use super::spatium_function::SpatiumFunction;
 
 pub struct PiecewiseDefinedFunction {
@@ -9,4 +10,14 @@ impl PiecewiseDefinedFunction {
         PiecewiseDefinedFunction { functions }
     }
 
+    pub fn filled_zeros(number_spatium: u64, length_spatium: f64) -> Self {
+        let mut functions = vec![];
+        for id in 0..number_spatium {
+            let x1 = id as f64 * length_spatium;
+            let x2 = x1 + length_spatium;
+            let spatium_function = SpatiumFunction::new(x1, x2, 0.0, 0.0);
+            functions.push(spatium_function);
+        }
+        PiecewiseDefinedFunction::new(functions)
+    }
 }
