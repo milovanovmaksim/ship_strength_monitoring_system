033572445e3aced2fad9ae59089978edae3c4580
milovanovmaksim <milovanov160386@gmail.com>
2023-10-22 15:49:16 +0300

    processed the condition

--- src/strength/ship/load/load.rs
+++ src/strength/ship/load/load.rs
@@ -114,30 +114,43 @@ impl Load {
             },
             LoadSpread::WithinManySpatiums => {
                 if self.load_start_coordinate() < ship_demensions.coordinate_aft() {
+                    let mut load_intensity = vec![];
                     debug!("Часть груза выступает за границу крайнего левого шпангоута. Координата начала груза {}", self.load_start_coordinate());
                     let load_length = self.load_start_coordinate().abs() - ship_demensions.coordinate_aft().abs();
                     let longitudinal_center_gravity = ship_demensions.coordinate_aft() - (load_length / 2.0);
                     let center_gravity = Point::new(longitudinal_center_gravity, self.center_gravity.y(), self.center_gravity.z());
                     let load_value = (load_length / self.length) * self.value;
                     let load_outside_leftmost_frame = Load::new(load_value, center_gravity, load_length);
+                    let frame_leftmost_outside_load_intensity = load_outside_leftmost_frame.load_intensity(ship_demensions);
 
                     let load_length = ship_demensions.coordinate_aft().abs() - self.load_end_coordinate().abs();
                     let longitudinal_center_gravity = self.load_end_coordinate() - load_length / 2.0;
                     let center_gravity = Point::new(longitudinal_center_gravity, self.center_gravity.y(), self.center_gravity.z());
                     let load_value = (load_length / self.length) * self.value;
                     let load_within_many_spatium = Load::new(load_value, center_gravity, load_length);
-                    todo!();
+                    let spatium_many_within_load_intensity = load_within_many_spatium.load_intensity(ship_demensions);
+                    load_intensity.extend(frame_leftmost_outside_load_intensity);
+                    load_intensity.extend(spatium_many_within_load_intensity);
+                    load_intensity
                 } else if self.load_end_coordinate() > ship_demensions.coordinate_bow() {
+                    let mut load_intensity = vec![];
                     debug!("Часть груза выступает за границу крайнего правого шпангоута. Координата конца груза {}", self.load_end_coordinate());
                     let load_length = self.load_end_coordinate() - ship_demensions.coordinate_bow();
                     let longitudinal_center_gravity = ship_demensions.coordinate_bow() + load_length / 2.0;
                     let center_gravity = Point::new(longitudinal_center_gravity, self.center_gravity.y(), self.center_gravity.z());
                     let load_value = (load_length / self.length) * self.value;
                     let load_outside_rightmost_frame = Load::new(load_value, center_gravity, load_length);
+                    let frame_rightmost_outside_load_intensity = load_outside_rightmost_frame.load_intensity(ship_demensions);
 
-
-                    todo!();
-
+                    let load_length = ship_demensions.coordinate_bow() - self.load_start_coordinate();
+                    let longitudinal_center_gravity = self.load_start_coordinate() + load_length / 2.0;
+                    let center_gravity = Point::new(longitudinal_center_gravity, self.center_gravity.y(), self.center_gravity.z());
+                    let load_value = (load_length / self.length) * self.value;
+                    let load_within_many_spatium = Load::new(load_value, center_gravity, load_length);
+                    let spatium_many_within_load_intensity = load_within_many_spatium.load_intensity(ship_demensions);
+                    load_intensity.extend(frame_rightmost_outside_load_intensity);
+                    load_intensity.extend(spatium_many_within_load_intensity);
+                    load_intensity
                 } else {
                     todo!();
                 }
@@ -160,11 +173,11 @@ impl Load {
         let spatium_end_index = self.spatium_end_index(ship_demensions);
         if self.load_start_coordinate() < ship_demensions.coordinate_aft() && self.load_end_coordinate() < ship_demensions.coordinate_aft() {
             debug!("Load.spread | The load is outside the leftmost frame. start index: {}, end index: {}", spatium_start_index, spatium_end_index);
-            debug!("Load.spread | The load component: {:#?}", self);
+            debug!("Load.spread | The load: {:#?}", self);
             LoadSpread::OutsideLeftmostFrame
         } else if self.load_start_coordinate() > ship_demensions.coordinate_bow() && self.load_end_coordinate() > ship_demensions.coordinate_bow()  {
             debug!("Load.spread | The load  is outside the rightmost frame. start index: {}, end index: {}", spatium_start_index, spatium_end_index);
-            debug!("Load.spread | The load : {:#?}. ShipDimensions: {:#?}", self, ship_demensions);
+            debug!("Load.spread | The load: {:#?}. ShipDimensions: {:#?}", self, ship_demensions);
             LoadSpread::OutsideRightmostFrame
         } else if self.load_end_coordinate() - self.load_start_coordinate() > ship_demensions.length_spatium() {
             debug!("Load.spread | The load spreads whithin many spatiums. start index: {}, end index: {}", spatium_start_index, spatium_end_index);
