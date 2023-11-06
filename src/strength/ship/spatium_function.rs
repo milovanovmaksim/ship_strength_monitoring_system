66f6f91bd90eb0335f25a62f58f4e128c2a4f55e
milovanovmaksim <milovanov160386@gmail.com>
2023-11-06 20:25:50 +0300

    add doc strings

--- src/strength/ship/spatium_function.rs
+++ src/strength/ship/spatium_function.rs
@@ -1,6 +1,7 @@
 use super::ship_dimensions::ShipDimensions;
 
-
+///
+/// Contains the results of any calculations for the spatium.
 #[derive(Debug)]
 pub struct SpatiumFunction {
     id: u64,
@@ -12,6 +13,8 @@ pub struct SpatiumFunction {
 }
 
 impl SpatiumFunction {
+    ///
+    /// Create new object.
     pub fn new(id: u64, x1: f64,
         x2: f64,
         f_x1: f64,
@@ -19,6 +22,9 @@ impl SpatiumFunction {
         SpatiumFunction { id, x1, x2, f_x1, f_x2 }
     }
 
+
+    ///
+    /// Create new object by id.
     pub fn from_id(id: i64, ship_demensions: &ShipDimensions, f_x1: f64, f_x2: f64) -> Self {
         let spatium_start_coordinate = ship_demensions.spatium_start_coordinate(id);
         let spatium_end_coordinate = ship_demensions.spatium_end_coordinate(id);
@@ -26,30 +32,42 @@ impl SpatiumFunction {
 
     }
 
+    /// Determines the integral of the function using a numerical method(trapezoidal method).
     pub fn integral(&self) -> f64 {
         ((self.f_x1 + self.f_x2) / 2.0) * (self.x2 - self.x1)
     }
 
+    ///
+    /// Return id of the object.
     pub fn id(&self) -> u64 {
         self.id
     }
 
+    ///
+    /// 
     pub fn x1(&self) -> f64 {
         self.x1
     }
 
+    ///
+    /// 
     pub fn x2(&self) -> f64 {
         self.x2
     }
 
+    ///
+    /// Return function value at a point x1.
     pub fn f_x1(&self) -> f64 {
         self.f_x1
     }
 
+    ///
+    /// Return function value at a point x2.
     pub fn f_x2(&self) -> f64 {
         self.f_x2
     }
 
+    /// Add curetn object with another.
     pub fn add(&self, term: SpatiumFunction) -> SpatiumFunction {
         let f_x1 = self.f_x1 + term.f_x1();
         let f_x2 = self.f_x2 + term.f_x2();
