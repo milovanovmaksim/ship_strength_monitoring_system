cfa6d22bd6728760a9a59b9f339051333646fdf6
milovanovmaksim <milovanov160386@gmail.com>
2023-11-07 23:42:31 +0300

    add deadweight_intensity method

--- src/strength/ship/deadweight/deadweight_intensity.rs
+++ src/strength/ship/deadweight/deadweight_intensity.rs
@@ -1,52 +1,19 @@
 use log::{warn, debug};
 use serde::Deserialize;
-use crate::{strength::ship::{spatium_function::SpatiumFunction, ship_dimensions::ShipDimensions, load::shipload::Shipload}, core::json_file::JsonFile};
+use crate::{strength::ship::{load::shiploads::Shiploads, spatium_functions::SpatiumFunctions}, core::json_file::JsonFile};
 
 #[derive(Deserialize, Debug)]
 pub struct DeadweightIntensity {
-    shiploads: Shiploads
+    shiploads: Shiploads,
 }
 
 
 impl DeadweightIntensity {
-    pub fn new(shiploads: Option<Vec<Shipload>>, ship_dimensions: ShipDimensions,) -> Self {
-        DeadweightIntensity { shiploads, ship_dimensions }
+    pub fn new(shiploads: Shiploads) -> Self {
+        DeadweightIntensity { shiploads }
     }
 
-    ///
-    /// Create the object from json file.
-    pub fn from_json_file(file_path: String) -> Result<Self, String> {
-        let json = JsonFile::new(file_path);
-        match json.content() {
-            Ok(content) => {
-                match serde_json::from_reader(content) {
-                    Ok(deadweight_intensity) => {
-                        debug!("DeadweightIntensity::from_json_file | DeadweightIntensity has been created sucessfuly. {:?}", deadweight_intensity);
-                        Ok(deadweight_intensity)
-                    },
-                    Err(err) => {
-                        warn!("DeadweightIntensity::from_json_file | error: {:?}.",err);
-                        return Err(err.to_string());
-                    }
-                }
-            },
-            Err(err) => {
-                warn!("DeadweightIntensity::from_json_file | error: {:?}.",err);
-                return Err(err);
-            }
-        }
-    }
-
-    pub fn deadweight_intensity(&self) -> Option<Vec<SpatiumFunction>> {
-        match &self.shiploads {
-            Some(loads) => {
-                let deadweight_intensity = self.spatiums_filled_zero();
-                for load in loads {
-                    let load_intensity = load.load_intensity(&self.ship_dimensions);
-                }
-                Some(deadweight_intensity)
-            },
-            None => { None }
-        }
+    pub fn deadweight_intensity(&self) -> SpatiumFunctions {
+        self.shiploads.intensity()
     }
 }
