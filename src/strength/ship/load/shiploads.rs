79ea9cb9f10aafdb22d5862a82cf41a5b67f5b76
milovanovmaksim <milovanov160386@gmail.com>
2023-11-07 17:50:17 +0300

    add sum method

--- src/strength/ship/load/shiploads.rs
+++ src/strength/ship/load/shiploads.rs
@@ -2,6 +2,9 @@ use crate::strength::ship::{spatium_functions::SpatiumFunctions, ship_dimensions
 
 use super::shipload::Shipload;
 
+
+///
+/// Сontains all the loads acting on the ship
 struct Shiploads {
     shiploads: Vec<Shipload>,
     dimensions: ShipDimensions,
@@ -9,19 +12,34 @@ struct Shiploads {
 
 
 impl Shiploads {
-    fn new(shiploads: Vec<Shipload>) -> Self {
-        Shiploads { shiploads }
+
+    ///
+    /// Create new object.
+    fn new(shiploads: Vec<Shipload>, dimensions: ShipDimensions) -> Self {
+        Shiploads { shiploads, dimensions }
     }
 
+    ///
+    /// Compute shiploads intensity.
     fn intensity(&self) -> SpatiumFunctions {
         let number_spatiums = self.dimensions.number_spatiums();
         let length_spatium = self.dimensions.length_spatium();
         let mut shaptium_functions = SpatiumFunctions::filled_zeros(number_spatiums, length_spatium);
 
-        for shipload in self.shiploads {
-            let spatium_function = shipload.load_intensity(&self.ship_demensions);
-            shaptium_functions.add_spatium_function(spatium_function);
+        for shipload in self.shiploads.iter() {
+            let load_intensity = shipload.load_intensity(&self.dimensions);
+            let _ = load_intensity.iter().map(|spatium_function| { shaptium_functions.add_spatium_function(spatium_function) });
         }
         shaptium_functions
+    }
+
+    ///
+    /// Returns the shiploads sum
+    fn sum(&self) -> f64 {
+        let mut sum = 0.0;
+        for shipload in self.shiploads.iter() {
+            sum += shipload.value();
+        }
+        sum
     }
 }
