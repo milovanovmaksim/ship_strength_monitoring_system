66fed5fe0b1c2b418fc01697829bed20579a6d02
milovanovmaksim <milovanov160386@gmail.com>
2023-11-08 16:26:12 +0300

    change main method

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod strength;
 mod core;
 mod tests;
 mod cross_section_properties;
+use core::visualisation::Visualisation;
 use std::env;
 
+use log::debug;
 use strength::ship::{ship_dimensions::ShipDimensions, deadweight::deadweight::Deadweight, load::shiploads::Shiploads};
 
 use crate::strength::ship::deadweight::deadweight_intensity::DeadweightIntensity;
@@ -17,26 +19,11 @@ fn main() {
     env_logger::init();
     let shiploads = Shiploads::from_json_file("./input_data/data.json".to_string());
     let deadweight_intensity = DeadweightIntensity::new(shiploads);
-    deadweight_intensity.deadweight_intensity();
+    let intensity = deadweight_intensity.deadweight_intensity().unwrap();
+    let spatium_length = 6.25;
+    let visualisation = Visualisation::new(
+        intensity, "Deadweght intensity".to_string(),
+         "Deadweight intensity".to_string(), spatium_length);
+    visualisation.visualize();
 
 }
-
-// Solution{
-//     EquBeam{
-//         CrossSections::from_csv_file(),
-//         BendingMoment{
-//             SheareForce{
-//                 TotalShipLoad {
-//                     BouyanLoad{
-//                         Ship::from_file(file_path),
-//                         BonjeanScale::from_file(file_path)
-//                     },
-//                     Displacment{
-//                         Deadweight::from_json_file(file_path),
-//                         Lightweight::from_json_file(file_path)
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
