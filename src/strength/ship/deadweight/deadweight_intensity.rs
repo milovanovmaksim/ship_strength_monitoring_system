5355b6ae0d1e17cf9df73ac7492bdba0d26bda81
milovanovmaksim <milovanov160386@gmail.com>
2023-11-08 15:01:54 +0300

    change deadweight_intensity method

--- src/strength/ship/deadweight/deadweight_intensity.rs
+++ src/strength/ship/deadweight/deadweight_intensity.rs
@@ -1,18 +1,27 @@
+use log::{debug, warn};
 use serde::Deserialize;
-use crate::strength::ship::{load::shiploads::Shiploads, spatium_functions::SpatiumFunctions};
+use crate::{strength::ship::{load::shiploads::Shiploads, spatium_functions::SpatiumFunctions}, core::json_file::JsonFile};
 
 #[derive(Deserialize, Debug)]
 pub struct DeadweightIntensity {
-    shiploads: Shiploads,
+    shiploads: Result<Shiploads, String>,
 }
 
 
 impl DeadweightIntensity {
-    pub fn new(shiploads: Shiploads) -> Self {
+    pub fn new(shiploads: Result<Shiploads, String>) -> Self {
         DeadweightIntensity { shiploads }
     }
 
-    pub fn deadweight_intensity(&self) -> SpatiumFunctions {
-        self.shiploads.intensity()
+    pub fn deadweight_intensity(&self) -> Result<SpatiumFunctions, String> {
+        match &self.shiploads {
+            Ok(shiploads) => {
+                Ok(shiploads.intensity())
+            }
+            Err(err) => {
+                debug!("DeadweightIntensity::deadweight_intensity | error: {:?}.",err);
+                Err(err.to_string())
+            }
+        }
     }
 }
