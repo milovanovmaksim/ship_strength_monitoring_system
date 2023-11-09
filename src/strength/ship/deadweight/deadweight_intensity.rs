3f86990ecf3b408169b782ef36443159f666e4a2
milovanovmaksim <milovanov160386@gmail.com>
2023-11-09 23:10:12 +0300

    delete the implementation of method

--- src/strength/ship/deadweight/deadweight_intensity.rs
+++ src/strength/ship/deadweight/deadweight_intensity.rs
@@ -1,27 +1,20 @@
 use log::warn;
 use serde::Deserialize;
-use crate::{strength::ship::{load::{shiploads::Shiploads, load_sharing::LoadSharing, shipload::Shipload, load_spread::LoadSpread}, spatium_functions::SpatiumFunctions, ship_dimensions::ShipDimensions}, core::json_file::JsonFile};
+use crate::strength::ship::{load::shiploads::Shiploads, ship_dimensions::ShipDimensions};
 
 #[derive(Deserialize, Debug)]
 pub struct DeadweightIntensity {
-    load_sharing: LoadSharing,
+    shiploads: Shiploads,
     ship_dimensions: ShipDimensions
 }
 
 
 impl DeadweightIntensity {
-    pub fn new(load_sharing: LoadSharing, ship_dimensions: ShipDimensions) -> Self {
-        DeadweightIntensity { load_sharing, ship_dimensions }
+    pub fn new(shiploads: Shiploads, ship_dimensions: ShipDimensions) -> Self {
+        DeadweightIntensity { shiploads, ship_dimensions }
     }
 
     pub fn deadweight_intensity(&self) {
-        let number_spatiums = self.ship_dimensions.number_spatiums();
-        let length_spatium = self.ship_dimensions.length_spatium();
-        let length_between_perpendiculars = self.ship_dimensions.length_between_perpendiculars();
-        let spatium_functions = SpatiumFunctions::filled_zeros(number_spatiums, length_spatium, length_between_perpendiculars);
-        let shiploads = self.load_sharing.shared_loads();
-        for shipload in shiploads.iter() {
-            todo!()
-        }
+        todo!()
     }
 }
