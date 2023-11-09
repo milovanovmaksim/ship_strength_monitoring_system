6a188692b48333a5c5b5a1607d981bf0b3899d2e
milovanovmaksim <milovanov160386@gmail.com>
2023-11-09 23:11:27 +0300

    delete the field

--- src/strength/ship/deadweight/deadweight_intensity.rs
+++ src/strength/ship/deadweight/deadweight_intensity.rs
@@ -1,17 +1,15 @@
-use log::warn;
 use serde::Deserialize;
 use crate::strength::ship::{load::shiploads::Shiploads, ship_dimensions::ShipDimensions};
 
 #[derive(Deserialize, Debug)]
 pub struct DeadweightIntensity {
     shiploads: Shiploads,
-    ship_dimensions: ShipDimensions
 }
 
 
 impl DeadweightIntensity {
-    pub fn new(shiploads: Shiploads, ship_dimensions: ShipDimensions) -> Self {
-        DeadweightIntensity { shiploads, ship_dimensions }
+    pub fn new(shiploads: Shiploads) -> Self {
+        DeadweightIntensity { shiploads }
     }
 
     pub fn deadweight_intensity(&self) {
--- src/strength/ship/load/load_sharing.rs
+++ src/strength/ship/load/load_sharing.rs
@@ -1,4 +1,4 @@
-use log::{debug};
+use log::debug;
 
 use crate::strength::ship::{ship_dimensions::ShipDimensions, load::shipload::Shipload};
 
