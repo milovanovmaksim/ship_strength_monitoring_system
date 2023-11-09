79f6da4691709d79dc522d4ce31cae6bc1500f48
milovanovmaksim <milovanov160386@gmail.com>
2023-11-10 01:31:06 +0300

    add deadweight intensity method

--- src/strength/ship/deadweight/deadweight_intensity.rs
+++ src/strength/ship/deadweight/deadweight_intensity.rs
@@ -1,18 +1,29 @@
 use serde::Deserialize;
-use crate::strength::ship::{load::shiploads::Shiploads, ship_dimensions::ShipDimensions};
+use crate::strength::ship::{load::shiploads::Shiploads, ship_dimensions::ShipDimensions, spatium_functions::SpatiumFunctions};
 
 #[derive(Deserialize, Debug)]
 pub struct DeadweightIntensity {
     shiploads: Shiploads,
+    ship_dimensions: ShipDimensions,
 }
 
 
 impl DeadweightIntensity {
-    pub fn new(shiploads: Shiploads) -> Self {
-        DeadweightIntensity { shiploads }
+    pub fn new(shiploads: Shiploads, ship_dimensions: ShipDimensions,) -> Self {
+        DeadweightIntensity { shiploads, ship_dimensions }
     }
 
-    pub fn deadweight_intensity(&self) {
-        todo!()
+    pub fn deadweight_intensity(&self) -> SpatiumFunctions {
+        let number_spatiums = self.ship_dimensions.number_spatiums();
+        let length_spatium = self.ship_dimensions.length_spatium();
+        let length_between_perpendiculars = self.ship_dimensions.length_between_perpendiculars();
+        let mut spatium_functions = SpatiumFunctions::filled_zeros(number_spatiums, length_spatium, length_between_perpendiculars);
+        for shipload in self.shiploads.shiploads() {
+            let shipload_intensity = shipload.shipload_intensity(&self.ship_dimensions);
+            for spatium_function in shipload_intensity.iter() {
+                spatium_functions.add_spatium_function(&spatium_function)
+            }
+        }
+        spatium_functions
     }
 }
