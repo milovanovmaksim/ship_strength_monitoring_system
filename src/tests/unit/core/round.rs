59933785058e2ee9dc4e4d580a9486ce9b2c80a1
milovanovmaksim <milovanov160386@gmail.com>
2023-11-12 16:25:16 +0300

    add test for round

---
+++ src/tests/unit/core/round.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod tests {
+    use crate::core::round::Round;
+
+    #[test]
+    fn test_round() {
+        assert_eq!(2.0, 2.0.my_round(1));
+        assert_eq!(2.1, 2.08.my_round(1));
+        assert_eq!(2.0, 2.04.my_round(1));
+        assert_eq!(24.25, 24.2499996.my_round(2));
+        assert_eq!(2.1, 2.05.my_round(1));
+        assert_eq!(3.0, 2.5.my_round(0));
+    }
+
+}
