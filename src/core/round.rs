51192b7b010b063be2875c6d1d200cbf80cde943
milovanovmaksim <milovanov160386@gmail.com>
2023-11-12 19:02:45 +0300

    change signature

--- src/core/round.rs
+++ src/core/round.rs
@@ -1,9 +1,10 @@
 pub(crate) trait Round {
-    fn my_round(&self, degets: u32) -> f64;
+    fn my_round(&self, degets: i32) -> f64;
 }
 
 impl Round for f64 {
-    fn my_round(&self, digits: u32) -> f64 {
-        (*self * 10_i32.pow(digits) as f64).round() / 10_i32.pow(digits) as f64
+    fn my_round(&self, digits: i32) -> f64 {
+        let scale = 10_f64.powi(digits);
+        (self * scale).round() / scale
     }
 }
