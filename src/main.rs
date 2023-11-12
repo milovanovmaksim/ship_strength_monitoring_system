038970fa91d0a4751d111032a015a41837d836ab
milovanovmaksim <milovanov160386@gmail.com>
2023-11-13 01:30:53 +0300

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,9 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "full");
     env_logger::init();
     let shiploads = Shiploads::from_json_file("./input_data/data.json".to_string()).unwrap();
-    let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
+    let ship_dimensions = ShipDimensions::new(125.0, 21, 0.6);
     let deadweight_intnesity = DeadweightIntensity::new(shiploads, ship_dimensions);
     let spatium_functions = deadweight_intnesity.spatium_functions();
-    let visualization = Visualisation::new(spatium_functions, "Deadweight intnesity".to_string(), "Deadweight intnesity".to_string(), 5.43);
+    let visualization = Visualisation::new(spatium_functions, "Deadweight intnesity".to_string(), "Deadweight intnesity".to_string(), 5.95);
     visualization.visualize();
 }
--- src/strength/ship/load/load_sharing.rs
+++ src/strength/ship/load/load_sharing.rs
@@ -26,22 +26,23 @@ impl<'a> LoadSharing<'a> {
         let x_2 = self.ship_dimensions.spatium_end_coordinate(spatium_start_index).my_round(2);
         let x_3 = self.ship_dimensions.spatium_start_coordinate(spatium_end_index).my_round(2);
         debug!("x_1 = {}, x_2 = {}, x_3 = {}, x_4 = {}", x_1, x_2, x_3, x_4);
-        if (x_1.abs() - x_2.abs()).abs() > 0.0 {
+        if (x_2 - x_1).abs() > 0.0 {
             let load = self.shipload.shared_shipload(x_1, x_2);
             shared_shiploads.push(load);
         }
-        if (x_4.abs() - x_3.abs()).abs() > 0.0 {
+        if (x_4 - x_3).abs() > 0.0 {
             let load = self.shipload.shared_shipload(x_3, x_4);
             shared_shiploads.push(load);
         }
         let mut load_start_coordinate = x_2;
         let mut load_end_coordinate = x_2 + self.ship_dimensions.length_spatium();
-        while load_end_coordinate <= x_3 {
+        loop {
             debug!("load_start_coordinate = {}, load_end_coordinate = {}", load_start_coordinate, load_end_coordinate);
             let load = self.shipload.shared_shipload(load_start_coordinate, load_end_coordinate);
             shared_shiploads.push(load);
             load_start_coordinate = load_end_coordinate;
             load_end_coordinate += self.ship_dimensions.length_spatium();
+            if load_end_coordinate >= x_3 { break; }
         }
         shared_shiploads
     }
