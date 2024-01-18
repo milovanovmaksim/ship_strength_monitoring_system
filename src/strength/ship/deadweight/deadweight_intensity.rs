badc6b97175efd599a3b328c8b54c674bd943e86
milovanovmaksim <milovanov160386@gmail.com>
2024-01-18 16:43:44 +0300

    delete debug

--- src/strength/ship/deadweight/deadweight_intensity.rs
+++ src/strength/ship/deadweight/deadweight_intensity.rs
@@ -19,7 +19,6 @@ impl DeadweightIntensity {
         let length_spatium = self.ship_dimensions.length_spatium();
         let length_between_perpendiculars = self.ship_dimensions.length_between_perpendiculars();
         let mut spatium_functions = SpatiumFunctions::filled_zeros(number_spatiums, length_spatium, length_between_perpendiculars);
-        debug!("SpatiumFunctions: {:#?}", spatium_functions);
         for shipload in self.shiploads.as_ref() {
             let shipload_intensity = ShiploadIntensity::new(shipload, &self.ship_dimensions);
             for spatium_function in shipload_intensity.spatium_functions().into_iter() {
