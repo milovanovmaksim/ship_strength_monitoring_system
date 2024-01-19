ca4e17c1f27ba8394258a7ec068335f8725739f7
milovanovmaksim <milovanov160386@gmail.com>
2024-01-19 21:02:16 +0300

    shared_loads_test

--- src/tests/unit/strength/load/load_sharing_test.rs
+++ src/tests/unit/strength/load/load_sharing_test.rs
@@ -3,7 +3,7 @@ mod tests {
     use std::{sync::Once, env};
     use log::debug;
 
-    use crate::strength::ship::{lightweight::lightweight::LightweightIntensity, ship_dimensions::ShipDimensions};
+    use crate::{strength::ship::{lightweight::lightweight::LightweightIntensity, ship_dimensions::ShipDimensions, load::{shipload::Shipload, load_sharing::LoadSharing}}, core::point::Point};
 
 
 
@@ -19,8 +19,28 @@ mod tests {
         )
     }
 
+    #[test]
+    fn number_shared_loads_test() {
+        let center_gravity = Point::new(29.29, 0.0, 0.0);
+        let shipload = Shipload::new(4.2, center_gravity, 10.21);
+        let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
+        let load_sharing = LoadSharing::new(&ship_dimensions, &shipload);
+        let shared_loads = load_sharing.shared_loads();
+        assert_eq!(3, shared_loads.len());
+    }
 
-
-
-
+    #[test]
+    fn shared_loads_test() {
+        let center_gravity = Point::new(29.29, 0.0, 0.0);
+        let shipload = Shipload::new(4.2, center_gravity, 10.21);
+        let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
+        let load_sharing = LoadSharing::new(&ship_dimensions, &shipload);
+        let test_shared_loads = vec![
+            Shipload::new(),
+            Shipload::new(),
+            Shipload::new()
+        ];
+        let shared_loads = load_sharing.shared_loads();
+        assert_eq!(3, shared_loads.len());
+    }
 }
