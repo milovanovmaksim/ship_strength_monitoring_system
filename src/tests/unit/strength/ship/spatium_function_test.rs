aad2f8a0882ecca829f022d5a8271ee1cc0f1d38
milovanovmaksim <milovanov160386@gmail.com>
2024-01-20 21:11:31 +0300

    add some tests

--- src/tests/unit/strength/ship/spatium_function_test.rs
+++ src/tests/unit/strength/ship/spatium_function_test.rs
@@ -2,7 +2,7 @@
 mod tests {
     use std::{sync::Once, env};
     use crate::{strength::ship::{ship_dimensions::ShipDimensions,
-        load::{shipload::Shipload, load_sharing::LoadSharing}}, core::point::Point};
+        load::{shipload::Shipload, load_sharing::LoadSharing}, spatium_function::SpatiumFunction}, core::point::Point};
 
 
 
@@ -18,5 +18,29 @@ mod tests {
         )
     }
 
-    
+    #[test]
+    fn from_id_test() {
+        call_once();
+        let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
+        let test_spatium_function = SpatiumFunction::new(0, -62.5, -56.25, 1.5, 1.5);
+        let spatium_function = SpatiumFunction::from_id(0, &ship_dimensions, 1.5, 1.5);
+        assert_eq!(test_spatium_function, spatium_function);
+    }
+
+    #[test]
+    fn add_test() {
+        let spatium_function = SpatiumFunction::new(0, -62.5, -56.25, 1.5, 1.5);
+        let other_spatium_function = SpatiumFunction::new(0, -62.5, -56.25, 1.5, 1.5);
+        let new_spatium_function = spatium_function.add(other_spatium_function);
+        let test_spatium_function = SpatiumFunction::new(0, -62.5, -56.25, 3.0, 3.0);
+        assert_eq!(test_spatium_function, new_spatium_function);
+    }
+
+    #[test]
+    fn integral_test() {
+        let spatium_function = SpatiumFunction::new(0, -62.5, -56.25, 1.5, 1.5);
+        let integral = spatium_function.integral();
+        let test_integral = 9.375;
+        assert_eq!(test_integral, integral);
+    }
 }
