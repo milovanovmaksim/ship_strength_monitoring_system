cd1198f6842350bec1d5d762f246fa312b49b5d7
milovanovmaksim <milovanov160386@gmail.com>
2024-01-24 20:17:19 +0300

    add a condition

--- src/strength/ship/load/shipload_intensity.rs
+++ src/strength/ship/load/shipload_intensity.rs
@@ -7,12 +7,13 @@ use crate::strength::ship::{load::{shipload::Shipload, load_sharing::LoadSharing
 /// Compute the shipload intensity.
 pub(crate) struct ShiploadIntensity<'a> {
     shipload: &'a Shipload,
-    ship_dimensions: &'a ShipDimensions
+    ship_dimensions: &'a ShipDimensions,
+    deadweight: f64
 }
 
 impl<'a> ShiploadIntensity<'a> {
-    pub fn new(shipload: &'a Shipload, ship_dimensions: &'a ShipDimensions) -> Self {
-        ShiploadIntensity { shipload, ship_dimensions }
+    pub fn new(shipload: &'a Shipload, ship_dimensions: &'a ShipDimensions, deadweight: f64) -> Self {
+        ShiploadIntensity { shipload, ship_dimensions , deadweight}
     }
 
     ///
@@ -34,6 +35,15 @@ impl<'a> ShiploadIntensity<'a> {
         if shipload.longitudinal_center_gravity() > self.ship_dimensions.coordinate_aft() && shipload.longitudinal_center_gravity() < self.ship_dimensions.coordinate_bow() {
             // TODO Грузы весом до 0,01 от дэдвейта могут быть равномерно разнесены
             //  по длине всей шпации независимо от действительного положения их центра тяжести.
+            if shipload.value() < self.deadweight * 0.01 {
+                debug!("Load.intensity | Вес груза распределяем на всю теоретическую шпацию. Shipload value = {} < deadweight = {}", self.shipload.value(), self.deadweight);
+                let spatium_start_index = self.ship_dimensions.spatium_index_by_coordinate(shipload.longitudinal_center_gravity());
+                let f_x = shipload.value() / self.ship_dimensions.length_spatium();
+                let spatium_function = SpatiumFunction::from_id(spatium_start_index, self.ship_dimensions, f_x, f_x);
+                let spatium_functions = vec![spatium_function];
+                debug!("Saptiums are under the load {:#?}", spatium_functions);
+                return spatium_functions;
+            }
             let max_intensity = |c_min: f64| { shipload.value() * (0.5 + (c_min / self.ship_dimensions.length_spatium())) / self.ship_dimensions.length_spatium() };
             let min_intensity = |c_min: f64| { shipload.value() * (0.5 - (c_min / self.ship_dimensions.length_spatium())) / self.ship_dimensions.length_spatium() };
             let (distance_left, distance_right) = shipload.distances_to_frames(self.ship_dimensions);
