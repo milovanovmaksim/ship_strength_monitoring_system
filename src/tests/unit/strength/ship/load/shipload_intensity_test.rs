cc1786700c59f3452b8d348c843825c4f6f0da6b
milovanovmaksim <milovanov160386@gmail.com>
2024-01-24 20:17:45 +0300

    add test

--- src/tests/unit/strength/ship/load/shipload_intensity_test.rs
+++ src/tests/unit/strength/ship/load/shipload_intensity_test.rs
@@ -24,7 +24,7 @@ mod tests {
         let center_gravity = Point::new(25.23, 0.0, 0.0);
         let shipload = Shipload::new(14.23, center_gravity, 10.21);
         let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
-        let shipload_intensity = ShiploadIntensity::new(&shipload, &ship_dimensions);
+        let shipload_intensity = ShiploadIntensity::new(&shipload, &ship_dimensions, 14.23);
         let spatium_functions = vec![
             SpatiumFunction::new(
                 13,
@@ -54,6 +54,24 @@ mod tests {
                 0.09,
                 0.09,
             ),
+        ];
+        let test_shipload_intensity = SpatiumFunctions::new(spatium_functions);
+        let shipload_intensity = shipload_intensity.spatium_functions();
+        assert_eq!(test_shipload_intensity, shipload_intensity);
+    }
+
+
+    #[test]
+    fn spatium_functions_test_2() {
+        // The Shipload value less than 0.001 * deadweight.
+        call_once();
+        let center_gravity = Point::new(25.23, 0.0, 0.0);
+        let shipload = Shipload::new(14.23, center_gravity, 10.21);
+        let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
+        let shipload_intensity = ShiploadIntensity::new(&shipload, &ship_dimensions, 1423.0);
+        let spatium_functions = vec![
+            SpatiumFunction::new(13, 18.75, 25.0, 1.09, 1.09),
+            SpatiumFunction::new(14, 25.0, 31.25, 1.19, 1.19)
         ];
         let test_shipload_intensity = SpatiumFunctions::new(spatium_functions);
         let shipload_intensity = shipload_intensity.spatium_functions();
