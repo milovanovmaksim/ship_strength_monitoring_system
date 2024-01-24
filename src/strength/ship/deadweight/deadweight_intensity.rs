2891868ac99f9cb4149c000ffc6bd97d1063deeb
milovanovmaksim <milovanov160386@gmail.com>
2024-01-24 20:16:50 +0300

    calculate deadweight

--- src/strength/ship/deadweight/deadweight_intensity.rs
+++ src/strength/ship/deadweight/deadweight_intensity.rs
@@ -17,8 +17,9 @@ impl DeadweightIntensity {
         let number_spatiums = self.ship_dimensions.number_spatiums();
         let length_between_perpendiculars = self.ship_dimensions.length_between_perpendiculars();
         let mut spatium_functions = SpatiumFunctions::filled_zeros(number_spatiums, length_between_perpendiculars);
+        let deadweight = self.shiploads.sum();
         for shipload in self.shiploads.as_ref() {
-            let shipload_intensity = ShiploadIntensity::new(shipload, &self.ship_dimensions);
+            let shipload_intensity = ShiploadIntensity::new(shipload, &self.ship_dimensions, deadweight);
             for spatium_function in shipload_intensity.spatium_functions().into_iter() {
                 spatium_functions.add_spatium_function(spatium_function)
             }
