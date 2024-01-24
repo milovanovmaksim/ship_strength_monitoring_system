fd9016989f019e7c3e30a5d138e3e9f4b476e7d1
milovanovmaksim <milovanov160386@gmail.com>
2024-01-24 22:08:58 +0300

    add DisplacementIntensity

---
+++ src/strength/ship/displacement/displacement_intensity.rs
@@ -0,0 +1,24 @@
+use crate::strength::ship::spatium_functions::SpatiumFunctions;
+
+struct DisplacementIntensity<'a> {
+    deadweight_intensity: &'a SpatiumFunctions,
+    lightweight_intnesity: &'a SpatiumFunctions,
+}
+
+
+impl<'a> DisplacementIntensity<'a> {
+    pub fn new(deadweight_intensity: &'a SpatiumFunctions, lightweight_intnesity: &'a SpatiumFunctions) -> Self {
+        DisplacementIntensity { deadweight_intensity, lightweight_intnesity }
+    }
+
+    pub fn spatium_functions(&self) -> SpatiumFunctions {
+        let deadweight_intensity = self.deadweight_intensity.as_ref();
+        let lightweight_intnesity = self.lightweight_intnesity.as_ref();
+        let spatium_functions = deadweight_intensity.iter().zip(lightweight_intnesity)
+            .map(|spatium_functions| {
+                spatium_functions.0.add(spatium_functions.1)
+            }).collect();
+        SpatiumFunctions::new(spatium_functions)
+
+    }
+}
