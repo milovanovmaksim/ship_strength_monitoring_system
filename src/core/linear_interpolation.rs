37746cc1df55d3e618e66df9cd250700184f63b6
milovanovmaksim <milovanov160386@gmail.com>
2024-02-11 17:28:09 +0300

    change condition

--- src/core/linear_interpolation.rs
+++ src/core/linear_interpolation.rs
@@ -23,7 +23,7 @@ impl LinearInterpolation {
     /// Примечание:
     ///    x_0 < x < x_1
     pub fn interpolated_value(&self, x: f64) -> Result<f64, String> {
-        if self.x_0 < x && x < self.x_1 {
+        if self.x_0 <= x && x <= self.x_1 {
             let f_x = self.f_x_0 + ((self.f_x_1 - self.f_x_0) / (self.x_1-self.x_0)) * (x - self.x_0);
             return Ok(f_x);
         }
