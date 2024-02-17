fc8a71f3d04716aeec68acb74fb981812b9294fd
milovanovmaksim <milovanov160386@gmail.com>
2024-02-17 17:29:57 +0300

    add frame_test

---
+++ src/tests/unit/strength/ship/buoyancy_load/frame_test.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    use crate::strength::ship::buoyancy_load::frame::Frame;
+
+
+    #[test]
+    fn area_by_draft_test() {
+        let drafts = 
+        let areas = 
+        let volumes = 
+        let masses = 
+        let abscissa
+        let frame = Frame::new(0, drafts, areas, volumes, masses, abscissa);
+        
+    }
+
+}
---
+++ src/tests/unit/strength/ship/buoyancy_load/mod.rs
@@ -0,0 +1,2 @@
+mod bonjean_scale_test;
+mod frame_test;
