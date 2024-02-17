8edae2c672959eaf7ec7a121a0ed1454cf0d2504
milovanovmaksim <milovanov160386@gmail.com>
2024-02-17 17:30:20 +0300

    add bonjean_scale_test

---
+++ src/tests/unit/strength/ship/buoyancy_load/bonjean_scale_test.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn frame_id_by_abscissa_test() {
+        
+    }
+
+}
--- src/tests/unit/strength/ship/mod.rs
+++ src/tests/unit/strength/ship/mod.rs
@@ -4,3 +4,4 @@ mod lightweight;
 mod spatium_function_test;
 mod spatium_functions_test;
 mod ship_dimensions_test;
+mod buoyancy_load;
