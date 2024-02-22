a070279ec489bdcb44a1518a086f6d46bf563441
milovanovmaksim <milovanov160386@gmail.com>
2024-02-22 19:17:22 +0300

    add some tests

--- src/tests/unit/strength/ship/buoyancy_load/frame_test.rs
+++ src/tests/unit/strength/ship/buoyancy_load/frame_test.rs
@@ -95,4 +95,30 @@ mod tests {
         assert_eq!(Err("Осадка превысила максимально допустимое значение для данного судна. Максимальная осадка: 13.3 [м].".to_string()), frame.massa_by_draft(15.0));
         assert_eq!(Err("Осадка судна не может быть отрицательной.".to_string()), frame.massa_by_draft(-1.0));
     }
+
+    #[test]
+    fn compare_frame_eq_test() {
+        let id = 6;
+        let drafts = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 13.3];
+        let areas = vec![32.30, 65.12, 98.09, 131.08, 164.08, 197.08, 230.08, 263.08, 296.08, 329.08, 362.08, 395.08, 428.08, 437.98];
+        let volumes = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
+        let masses = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
+        let abscissa =  -25.0;
+        let frame = Frame::new(id, drafts.clone(), areas.clone(), volumes.clone(), masses.clone(), abscissa);
+        let other_frame = Frame::new(id, drafts, areas, volumes, masses, abscissa);
+        assert!(frame == other_frame);
+    }
+
+    #[test]
+    fn compare_frame_ne_test() {
+        let id = 6;
+        let drafts = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 13.3];
+        let areas = vec![32.30, 65.12, 98.09, 131.08, 164.08, 197.08, 230.08, 263.08, 296.08, 329.08, 362.08, 395.08, 428.08, 437.98];
+        let volumes = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
+        let masses = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
+        let abscissa =  -25.0;
+        let frame = Frame::new(id, drafts.clone(), areas.clone(), volumes.clone(), masses.clone(), abscissa);
+        let other_frame = Frame::new(id, drafts, areas, volumes, masses, abscissa+1.0);
+        assert!(frame != other_frame);
+    }
 }
