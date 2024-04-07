a7a0e3034f75a3cf28069c00282347c6dc506142
milovanovmaksim <milovanov160386@gmail.com>
2024-04-07 17:09:09 +0300

    refact struct Frame

--- src/strength/ship/buoyancy_load/frame.rs
+++ src/strength/ship/buoyancy_load/frame.rs
@@ -2,6 +2,8 @@ use log::error;
 use serde::Deserialize;
 use crate::core::{binary_search::BinarySearch, linear_interpolation::LinearInterpolation};
 
+use super::bonjean_scale_data_type::BonjeanScaleDataType;
+
 
 
 ///
@@ -82,14 +84,10 @@ impl Frame {
     }
 
     ///
-    /// Возвращает погруженную площадь шпангоута для заданной осадки. [м^2]
-    /// Если такой осадки нет, линейно интерполирует площадь погруженного шпангоута,
-    /// имея в распоряжении две известные площади шпангоутов для промежуточных осадок между
-    /// которыми лежит заданная осадка.
-    /// Parametrs:
-    ///     draft: осадка для которой нужно вернуть погруженную площадь шпангоута.
-    ///     Параметр draft  не должен выходить за пределы допустимого диапазона осадки судна.
-    /// # Examples
+    /// Возвращает данные масштаба Бонжана для заданной осадки.
+    /// Если такой осадки нет, линейно интерполирует данные масштаба Бонжана шпангоута
+    /// для промежуточных осадок, между которыми лежит заданная осадка.
+    /// # Example
     /// ```
     /// let id = 6;
     /// let drafts = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 13.3];
@@ -97,31 +95,27 @@ impl Frame {
     /// let volumes = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
     /// let masses = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
     /// let abscissa =  -25.0;
-    /// let frame = Frame::new(id, drafts, areas, volumes, masses, abscissa);
-    /// assert_eq!(0.0, frame.area_by_draft(0.5).unwrap());
-    /// assert_eq!(65.12, frame.area_by_draft(2.0).unwrap());
-    /// assert_eq!(32.3, frame.area_by_draft(1.0).unwrap());
-    /// // Линейно интерполирует погруженную площадь шпангоута между осадками 2.0 и 3.0 метра.
-    /// assert_eq!(81.605, frame.area_by_draft(2.5).unwrap());
-    /// assert_eq!(Err("Осадка превысила максимально допустимое значение для данного судна. Максимальная осадка: 13.3 [м].".to_string()), frame.area_by_draft(15.0));
+    /// let frame = Frame::new(id, drafts.clone(), areas.clone(), volumes, masses, abscissa).unwrap();
+    /// assert_eq!(0.0, frame.data_by_draft(0.5, BonjeanScaleDataType::Area).unwrap());
+    /// for i in 0..drafts.len() {
+    ///    let draft = *drafts.get(i).unwrap();
+    ///    assert_eq!(*areas.get(i).unwrap(), frame.data_by_draft(draft, BonjeanScaleDataType::Area).unwrap());
+    /// }
+    /// //Линейно интерполирует погруженную площадь шпангоута между осадками 2.0 и 3.0 метра.
+    /// assert_eq!(81.605, frame.data_by_draft(2.5, BonjeanScaleDataType::Area).unwrap());
     /// ```
-    pub fn area_by_draft(&self, draft: f64) -> Result<f64, String> {
-        match self.data_by_draft(draft, &self.areas) {
-            Ok(area) => { Ok(area) },
-            Err(error) => {
-                error!("Frame::area_by_draft | error: {}", error);
-                Err(error)
-            }
-        }
-    }
-
-    ///
-    /// Возвращает данные масштаба Бонжана для заданной осадки.
-    fn data_by_draft(&self, draft: f64, data: &Vec<f64>) -> Result<f64, String> {
+    pub fn data_by_draft(&self, draft: f64, type_data: BonjeanScaleDataType) -> Result<f64, String> {
         let min_draft = *self.drafts.first().unwrap();
         if draft < min_draft {
             return Ok(0.0);
         }
+        let data = {
+            match type_data {
+                BonjeanScaleDataType::Area => { &self.areas }
+                BonjeanScaleDataType::Volume => { &self.volumes },
+                BonjeanScaleDataType::Massa => { &self.masses },
+            }
+        };
         match self.validate_draft(draft) {
             Ok(_) => {
                 match self.drafts.custom_binary_search(draft) {
@@ -159,78 +153,5 @@ impl Frame {
             return Err(format!("Осадка превысила максимально допустимое значение для данного судна. Максимальная осадка: {} [м].", max_draft));
         }
         Ok(())
-    }
-
-
-    ///
-    /// Возвращает погруженный объем шпангоута для заданной осадки. [м^3]
-    /// Если такой осадки нет, линейно интерполирует объем погруженного шпангоута,
-    /// имея в распоряжении два известных объема шпангоутов для промежуточных осадок между
-    /// которыми лежит заданная осадка.
-    /// Parametrs:
-    ///     draft: осадка для которой нужно вернуть погруженный объем шпангоута.
-    ///     Параметр draft  не должен выходить за пределы допустимого диапазона осадки судна.
-    /// # Examples
-    /// ```
-    /// let id = 6;
-    /// let drafts = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 13.3];
-    /// let areas = vec![32.30, 65.12, 98.09, 131.08, 164.08, 197.08, 230.08, 263.08, 296.08, 329.08, 362.08, 395.08, 428.08, 437.98];
-    /// let volumes = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
-    /// let masses = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
-    /// let abscissa =  -25.0;
-    /// let frame = Frame::new(id, drafts, areas, volumes, masses, abscissa);
-    /// assert_eq!(0.0, frame.volume_by_draft(0.5).unwrap());
-    /// assert_eq!(379.52, frame.volume_by_draft(1.0).unwrap());
-    /// assert_eq!(765.20, frame.volume_by_draft(2.0).unwrap());
-    /// assert_eq!(5146.22, frame.volume_by_draft(13.3).unwrap());
-    /// assert_eq!(5029.90, frame.volume_by_draft(13.0).unwrap());
-    /// // Линейно интерполирует погруженный объем шпангоута между осадками 2.0 и 3.0 метра.
-    /// assert_eq!(958.855, frame.volume_by_draft(2.5).unwrap());
-    /// assert_eq!(Err("Осадка превысила максимально допустимое значение для данного судна. Максимальная осадка: 13.3 [м].".to_string()), frame.volume_by_draft(15.0));
-    /// ```
-    pub fn volume_by_draft(&self, draft: f64) -> Result<f64, String> {
-        match self.data_by_draft(draft, &self.volumes) {
-            Ok(volume) => { Ok(volume) },
-            Err(error) => {
-                error!("Frame::volume_by_draft | error: {}", error);
-                Err(error)
-            }
-        }
-    }
-
-    ///
-    /// Возвращает погруженную массу шпангоута для заданной осадки. [т]
-    /// Если такой осадки нет, линейно интерполирует массу погруженного шпангоута,
-    /// имея в распоряжении две известные массы шпангоутов для промежуточных осадок между
-    /// которыми лежит заданная осадка.
-    /// Parametrs:
-    ///     draft: осадка для которой нужно вернуть погруженную массу шпангоута.
-    ///     Параметр draft  не должен выходить за пределы допустимого диапазона осадки судна.
-    /// # Examples
-    /// ```
-    /// let id = 6;
-    /// let drafts = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 13.3];
-    /// let areas = vec![32.30, 65.12, 98.09, 131.08, 164.08, 197.08, 230.08, 263.08, 296.08, 329.08, 362.08, 395.08, 428.08, 437.98];
-    /// let volumes = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
-    /// let masses = vec![379.52, 765.20, 1152.51, 540.16, 1927.90, 2315.65, 2703.40, 3091.15, 3478.90, 3866.65, 4254.40, 4642.15, 5029.90, 5146.22];
-    /// let abscissa =  -25.0;
-    /// let frame = Frame::new(id, drafts, areas, volumes, masses, abscissa);
-    /// assert_eq!(0.0, frame.massa_by_draft(0.5).unwrap());
-    /// assert_eq!(379.52, frame.massa_by_draft(1.0).unwrap());
-    /// assert_eq!(765.20, frame.massa_by_draft(2.0).unwrap());
-    /// assert_eq!(5146.22, frame.massa_by_draft(13.3).unwrap());
-    /// assert_eq!(5029.90, frame.massa_by_draft(13.0).unwrap());
-    /// // Линейно интерполирует погруженную массу шпангоута между осадками 2.0 и 3.0 метра.
-    /// assert_eq!(958.855, frame.massa_by_draft(2.5).unwrap());
-    /// assert_eq!(Err("Осадка превысила максимально допустимое значение для данного судна. Максимальная осадка: 13.3 [м].".to_string()), frame.massa_by_draft(15.0));
-    /// ```
-    pub fn massa_by_draft(&self, draft: f64) -> Result<f64, String> {
-        match self.data_by_draft(draft, &self.masses) {
-            Ok(massa) => { Ok(massa) },
-            Err(error) => {
-                error!("Frame::massa_by_draft | error: {}", error);
-                Err(error)
-            }
-        }
     }
 }
