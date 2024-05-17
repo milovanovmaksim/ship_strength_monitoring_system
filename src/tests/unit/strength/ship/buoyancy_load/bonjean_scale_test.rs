fc60ccb65ee68c8a7a6264c3d4594954d3f3182d
milovanovmaksim <milovanov160386@gmail.com>
2024-05-18 00:43:22 +0300

    delete unnecessary methods

--- src/tests/unit/strength/ship/buoyancy_load/bonjean_scale_test.rs
+++ src/tests/unit/strength/ship/buoyancy_load/bonjean_scale_test.rs
@@ -18,29 +18,19 @@ mod tests {
     }
 
 
-    fn bonjean_scale(file_path: String) -> Result<BonjeanScale, String> {
-        let frames = frames(file_path)?;
-        let ship_dimensions = ship_dimensions();
-        BonjeanScale::new(frames, ship_dimensions)
-    }
-
     fn frames(file_path: String) -> Result<Vec<Frame>, String> {
         let json = JsonFile::new(file_path);
         let content = json.content()?;
         serde_json::from_reader(content).map_err(|err| { err.to_string() })
     }
 
-    fn ship_dimensions() -> ShipDimensions {
-        ShipDimensions::new(125.0, 20, 0.6)
-    }
-
     #[test]
     fn underwater_area_frame_ok_test() {
         call_once();
         let file_path = "src/tests/unit/strength/ship/buoyancy_load/test_data/frames.json".to_string();
-        let bonjean_scale = bonjean_scale(file_path.clone()).unwrap();
         let frames = frames(file_path).unwrap();
-        let ship_dimensions = ship_dimensions();
+        let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
+        let bonjean_scale = BonjeanScale::new(frames, ship_dimensions).unwrap();
         let underwater_area_frame = bonjean_scale.underwater_area_frame(-56.25, 1.0).unwrap();
         assert_eq!(7.04, underwater_area_frame);
 
