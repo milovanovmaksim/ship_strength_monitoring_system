2c59812d83a981ea0b0e26fca458e94ceec7a29f
milovanovmaksim <milovanov160386@gmail.com>
2024-05-31 19:54:39 +0300

    implement ship_underwater_volume method

--- src/strength/ship/buoyancy_load/bonjean_scale.rs
+++ src/strength/ship/buoyancy_load/bonjean_scale.rs
@@ -11,23 +11,23 @@ use super::{bonjean_scale_data_type::BonjeanScaleDataType, frames::Frames};
 ///     shipdimensions - размерения судна.
 pub(crate) struct BonjeanScale {
     frames: Frames,
-    shipdimensions: ShipDimensions
+    ship_dimensions: ShipDimensions
 }
 
 impl BonjeanScale {
     pub fn new(frames: Frames, shipdimensions: ShipDimensions) -> Self {
-        BonjeanScale { frames, shipdimensions }
+        BonjeanScale { frames, ship_dimensions: shipdimensions }
     }
 
 
     fn validate_abscissa(&self, abscissa: f64) -> Result<(), String> {
-        if abscissa < self.shipdimensions.coordinate_aft() {
+        if abscissa < self.ship_dimensions.coordinate_aft() {
             return Err(format!("Абсцисса вышла за пределы координаты кормы судна. Координа кормы: {}. Передано значение: {}",
-                self.shipdimensions.coordinate_aft(), abscissa));
+                self.ship_dimensions.coordinate_aft(), abscissa));
         }
-        if abscissa > self.shipdimensions.coordinate_bow() {
+        if abscissa > self.ship_dimensions.coordinate_bow() {
             return Err(format!("Абсцисса вышла за пределы координаты носа судна. Координа носа: {}. Передано значение: {}",
-                self.shipdimensions.coordinate_bow(), abscissa));
+                self.ship_dimensions.coordinate_bow(), abscissa));
         }
         Ok(())
     }
@@ -116,13 +116,36 @@ impl BonjeanScale {
 
 
     ///
-    /// Возвращает погруженный объем судна в воду (объемное водоизмещение) для заданных осадок носа и кормы.
+    /// Возвращает погруженный объем судна в воду (объемное водоизмещение) для заданных осадок носа и кормы. [м^3]
     /// Parameters:
     ///     aft_draft - осадка кормы [м],
     ///     nose_draft - осадка носа [м].
-    pub fn ship_underwater_volume(&self, aft_draft: f64, nose_draft: f64) -> f64 {
-        todo!();
-
-
+    pub fn ship_underwater_volume(&self, aft_draft: f64, nose_draft: f64) -> Result<f64, String> {
+        let half_length_spatium = self.ship_dimensions.length_spatium() / 2.0;
+        let coordinate_aft = self.ship_dimensions.coordinate_aft();
+        let mut abscissa = coordinate_aft + half_length_spatium;
+        let coordinate_bow = self.ship_dimensions.coordinate_bow();
+        let linear_interpolation = LinearInterpolation::new(aft_draft, nose_draft,
+                                                                                 coordinate_aft, coordinate_bow);
+        let mut ship_underwater_volume = 0.0;
+        for _ in 0..self.ship_dimensions.number_spatiums() {
+            match linear_interpolation.interpolated_value(abscissa) {
+                Ok(draft) => {
+                    match self.underwater_volume_frame(abscissa, draft) {
+                        Ok(underwater_volume_frame) => { ship_underwater_volume += underwater_volume_frame; }
+                        Err(err) => {
+                            error!("BonjeanScale::ship_underwater_volume | error: {}", err);
+                            return Err(err);
+                        }
+                    }
+                }
+                Err(err) => {
+                    error!("BonjeanScale::ship_underwater_volume | error: {}", err);
+                    return Err(err);
+                }
+            }
+            abscissa += self.ship_dimensions.length_spatium();
+        }
+        Ok(ship_underwater_volume)
     }
 }
