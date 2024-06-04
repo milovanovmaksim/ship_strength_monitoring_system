9712501b1bc0e0c00b77a8fbcf1b033d8105d4eb
milovanovmaksim <milovanov160386@gmail.com>
2024-06-04 10:01:01 +0300

    implement displacement method

--- src/strength/ship/displacement/displacement.rs
+++ src/strength/ship/displacement/displacement.rs
@@ -1,10 +1,13 @@
-use crate::strength::ship::buoyancy_load::bonjean_scale::BonjeanScale;
+use log::error;
+
+use crate::{core::linear_interpolation::LinearInterpolation, strength::ship::{buoyancy_load::bonjean_scale::BonjeanScale, ship_dimensions::ShipDimensions}};
 
 
 ///
 /// Объемное водоизмещение судна.
 pub struct Displacement<'a> {
-    bonjean_scale: &'a BonjeanScale<'a>
+    bonjean_scale: &'a BonjeanScale<'a>,
+    ship_dimensions: ShipDimensions
 }
 
 
@@ -12,13 +15,40 @@ impl<'a> Displacement<'a> {
 
     ///
     /// Конструктор.
-    pub fn new(bonjean_scale: &'a BonjeanScale) -> Self {
-        Displacement { bonjean_scale }
+    pub fn new(bonjean_scale: &'a BonjeanScale, ship_dimensions: ShipDimensions) -> Self {
+        Displacement { bonjean_scale, ship_dimensions }
     }
 
     ///
-    /// Возвращает объемное водоизмещение судна [м^3]
-    pub fn displacement(&self) -> Result<f64, String> {
-        todo!();
+    /// Возвращает объемное водоизмещение судна от осадки судна. [м^3]
+    pub fn displacement(&self, aft_draft: f64, nose_draft: f64) -> Result<f64, String> {
+        let length_spatium = self.ship_dimensions.length_spatium();
+        let coordinate_aft = self.ship_dimensions.coordinate_aft();
+        let mut abscissa = coordinate_aft + length_spatium / 2.0;
+        let coordinate_bow = self.ship_dimensions.coordinate_bow();
+        let linear_interpolation = LinearInterpolation::new(aft_draft, nose_draft,
+                                                                                 coordinate_aft, coordinate_bow);
+        let mut ship_underwater_volume = 0.0;
+        for _ in 0..self.ship_dimensions.number_spatiums() {
+            match linear_interpolation.interpolated_value(abscissa) {
+                Ok(draft) => {
+                    match self.bonjean_scale.frame_underwater_volume(abscissa, draft) {
+                        Ok(frame_underwater_volume) => {
+                            ship_underwater_volume += frame_underwater_volume;
+                        }
+                        Err(err) => {
+                            error!("LCB::lcb | error: {}", err);
+                            return Err(err);
+                        }
+                    }
+                }
+                Err(err) => {
+                    error!("LCB::lcb | error: {}", err);
+                    return Err(err);
+                }
+            }
+            abscissa += length_spatium
+        }
+        Ok(ship_underwater_volume)
     }
 }
