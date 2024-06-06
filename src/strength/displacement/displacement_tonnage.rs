7a6ffedcaa747c0ee20183884378437a443505a5
milovanovmaksim <milovanov160386@gmail.com>
2024-06-06 22:06:40 +0300

    implement DisplacementTonnage

--- src/strength/displacement/displacement_tonnage.rs
+++ src/strength/displacement/displacement_tonnage.rs
@@ -4,3 +4,14 @@ pub(crate) struct DisplacementTonnage {
     lightweight: Lightweight,
     deadweight: Deadweight
 }
+
+
+impl DisplacementTonnage {
+    pub fn new(lightweight: Lightweight, deadweight: Deadweight) -> Self {
+        DisplacementTonnage { lightweight, deadweight }
+    }
+
+    pub fn displacement_tonnage(&self) -> f64 {
+        self.lightweight.lightweight() + self.deadweight.deadweight()
+    }
+}
