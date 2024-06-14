385df383b3ecb282ff22a5da2db769ee120e8c0a
milovanovmaksim <milovanov160386@gmail.com>
2024-06-14 15:05:02 +0300

    add mod physical_constants

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,7 @@
 pub(crate) mod binary_search;
 pub(crate) mod json_file;
 pub(crate) mod linear_interpolation;
+pub(crate) mod physical_constants;
 pub(crate) mod point;
 pub(crate) mod round;
 pub(crate) mod system_of_units;
