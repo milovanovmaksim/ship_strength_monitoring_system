337e6bbf6a5f61b6b84dafd4947851b1613bac7f
milovanovmaksim <milovanov160386@gmail.com>
2024-07-22 00:06:22 +0300

    refactore ShipTrimming struct

--- src/strength/buoyancy_intensity/ship_trimming.rs
+++ src/strength/buoyancy_intensity/ship_trimming.rs
@@ -54,16 +54,12 @@ impl<'a> ShipTrimming<'a> {
     /// Parameters:
     ///     displacement - водоизмещение судна при текущей схеме загрузки[м^3];
     ///     calc_disp - расчетное объемное водоизмещение судна [м^3].
-    fn trim_achieved(
-        &self,
-        displacement: f64,
-        calc_disp: f64,
-        lcg: f64,
-        lcb: f64,
-        lbp: f64,
-    ) -> bool {
-        self.error(calc_disp, displacement) <= 0.01
-            && (lcg.abs() - lcb.abs()).abs() <= (0.001 * lbp)
+    fn trim_achieved(&self, calc_disp: f64, lcb: f64, lbp: f64) -> Result<bool, String> {
+        let displacement_tonnage = self.displacement_tonnage.displacement_tonnage();
+        let displacement = self.displacement.displacement_by_mass(displacement_tonnage);
+        let lcg = self.lcg.lcg()?;
+        Ok(self.error(calc_disp, displacement) <= 0.01
+            && (lcg.abs() - lcb.abs()).abs() <= (0.001 * lbp))
     }
     ///
     /// Расчет процентного различия между двумя числами.
@@ -75,14 +71,17 @@ impl<'a> ShipTrimming<'a> {
     /// Выводит информацию о удифферентовки судна.
     fn solution_information(
         &self,
+        i: u32,
         aft_draft: f64,
         nose_draft: f64,
-        lcg: f64,
-        lcb: f64,
         lbp: f64,
-        displacement: f64,
         calc_disp: f64,
-    ) {
+    ) -> Result<(), String> {
+        let displacement_tonnage = self.displacement_tonnage.displacement_tonnage();
+        let displacement = self.displacement.displacement_by_mass(displacement_tonnage);
+        let lcg = self.lcg.lcg()?;
+        let lcb = self.lcb.lcb(aft_draft, nose_draft)?;
+        info!("---------- Итерация № {i} ---------- ");
         info!("aft_draft = {} м, nose_draft = {} м", aft_draft, nose_draft);
         info!("mean_draft = {} м", 0.5 * (aft_draft + nose_draft));
         info!("lcg = {} м, lcb = {} м", lcg, lcb);
@@ -108,6 +107,7 @@ impl<'a> ShipTrimming<'a> {
                 self.error(displacement, calc_disp)
             );
         }
+        Ok(())
     }
     ///
     /// Определяет коэффициент подобия треугольника.
@@ -126,6 +126,53 @@ impl<'a> ShipTrimming<'a> {
         b / a
     }
 
+    fn trimming(&self, lbp: f64, mean_draft: f64, lcf: f64) -> Result<(f64, f64), String> {
+        let mut max_draft = self.hydrostatic_curves.max_draft();
+        let mut min_draft = self.hydrostatic_curves.min_draft();
+        let mut nose_draft = mean_draft;
+        let mut aft_draft = mean_draft;
+        let mut lcb = self.lcb.lcb(aft_draft, nose_draft)?;
+        let lcg = self.lcg.lcg()?;
+        let mut j = 0;
+        let similarity_coefficient = self.similarity_coefficient(lcf, lbp);
+        while (lcg.abs() - lcb.abs()).abs() > 0.001 * lbp && j < 100 {
+            if lcg < lcb {
+                // Поворот ВЛ по часовой стрелки.
+                if aft_draft > nose_draft {
+                    max_draft = nose_draft;
+                    nose_draft = (max_draft + min_draft) / 2.0;
+                    aft_draft = ((mean_draft - nose_draft) / similarity_coefficient) + mean_draft;
+                } else if aft_draft < nose_draft {
+                    max_draft = nose_draft;
+                    nose_draft = (min_draft + max_draft) / 2.0;
+                    aft_draft = mean_draft - (nose_draft - mean_draft) / similarity_coefficient;
+                } else {
+                    min_draft = mean_draft;
+                    aft_draft = (min_draft + max_draft) / 2.0;
+                    nose_draft = mean_draft - (aft_draft - mean_draft) * similarity_coefficient;
+                }
+            } else if lcg > lcb {
+                // Поворот ВЛ против часовой.
+                if aft_draft > nose_draft {
+                    min_draft = nose_draft;
+                    nose_draft = (max_draft + min_draft) / 2.0;
+                    aft_draft = ((mean_draft - nose_draft) / similarity_coefficient) + mean_draft;
+                } else if aft_draft < nose_draft {
+                    min_draft = nose_draft;
+                    nose_draft = (max_draft + min_draft) / 2.0;
+                    aft_draft = mean_draft - (nose_draft - mean_draft) / similarity_coefficient;
+                } else {
+                    min_draft = mean_draft;
+                    nose_draft = (max_draft + min_draft) / 2.0;
+                    aft_draft = mean_draft - (nose_draft - mean_draft) / similarity_coefficient;
+                }
+            }
+            lcb = self.lcb.lcb(aft_draft, nose_draft)?;
+            j += 1;
+        }
+        Ok((aft_draft, nose_draft))
+    }
+
     ///
     /// Удифферентовка судна методом последовательных приближений.
     /// Возвращает осадку кормы и носа судна (aft_draft, nose_draft).
@@ -138,7 +185,6 @@ impl<'a> ShipTrimming<'a> {
             .hydrostatic_curves
             .mean_draft(displacement_tonnage)?
             .unwrap();
-        let lcg = self.lcg.lcg()?;
         let displacement = self.displacement.displacement_by_mass(displacement_tonnage);
         let lcf = self
             .hydrostatic_curves
@@ -147,85 +193,22 @@ impl<'a> ShipTrimming<'a> {
         let mut max_draft_d = self.hydrostatic_curves.max_draft();
         let mut min_draft_d = self.hydrostatic_curves.min_draft();
         let lbp = ship_dimensions.lbp();
-        let similarity_coefficient = self.similarity_coefficient(lcf, lbp);
-        for _ in 0..50 {
-            let mut i = 1;
-            let mut max_draft = self.hydrostatic_curves.max_draft();
-            let mut min_draft = self.hydrostatic_curves.min_draft();
-            let mut nose_draft = mean_draft;
-            let mut aft_draft = mean_draft;
-            let mut lcb = self.lcb.lcb(aft_draft, nose_draft)?;
-            let mut calc_disp = self
+        for i in 0..50 {
+            let (aft_draft, nose_draft) = self.trimming(lbp, mean_draft, lcf)?;
+            let calc_disp = self
                 .displacement
                 .displacement_by_drafts(aft_draft, nose_draft)?;
-            if self.trim_achieved(displacement, calc_disp, lcg, lcb, lbp) {
-                info!("Удифферентовка судна на тихой воде достигнута.");
-                return Ok((aft_draft.my_round(2), nose_draft.my_round(2)));
-            }
-            while (lcg.abs() - lcb.abs()).abs() > 0.001 * lbp && i <= 51 {
-                if lcg < lcb {
-                    // Поворот ВЛ по часовой стрелки.
-                    if aft_draft > nose_draft {
-                        max_draft = nose_draft;
-                        nose_draft = (max_draft + min_draft) / 2.0;
-                        aft_draft =
-                            ((mean_draft - nose_draft) / similarity_coefficient) + mean_draft;
-                    } else if aft_draft < nose_draft {
-                        max_draft = nose_draft;
-                        nose_draft = (min_draft + max_draft) / 2.0;
-                        aft_draft = mean_draft - (nose_draft - mean_draft) / similarity_coefficient;
-                    } else {
-                        min_draft = mean_draft;
-                        aft_draft = (min_draft + max_draft) / 2.0;
-                        nose_draft = mean_draft - (aft_draft - mean_draft) * similarity_coefficient;
-                    }
-                } else if lcg > lcb {
-                    // Поворот ВЛ против часовой.
-                    if aft_draft > nose_draft {
-                        min_draft = nose_draft;
-                        nose_draft = (max_draft + min_draft) / 2.0;
-                        aft_draft =
-                            ((mean_draft - nose_draft) / similarity_coefficient) + mean_draft;
-                    } else if aft_draft < nose_draft {
-                        min_draft = nose_draft;
-                        nose_draft = (max_draft + min_draft) / 2.0;
-                        aft_draft = mean_draft - (nose_draft - mean_draft) / similarity_coefficient;
-                    } else {
-                        min_draft = mean_draft;
-                        nose_draft = (max_draft + min_draft) / 2.0;
-                        aft_draft = mean_draft - (nose_draft - mean_draft) / similarity_coefficient;
-                    }
-                }
-                lcb = self.lcb.lcb(aft_draft, nose_draft)?;
-                calc_disp = self
-                    .displacement
-                    .displacement_by_drafts(aft_draft, nose_draft)?;
-                info!("---------------Итерация №{i}-------------");
-                self.solution_information(
-                    aft_draft,
-                    nose_draft,
-                    lcg,
-                    lcb,
-                    lbp,
-                    displacement,
-                    calc_disp,
-                );
-                if self.trim_achieved(displacement, calc_disp, lcg, lcb, lbp) {
-                    info!("Удифферентовка судна на тихой воде достигнута.");
-                    info!("---------------Конец итерации {i}-------------\n");
-                    return Ok((aft_draft.my_round(2), nose_draft.my_round(2)));
-                } else {
-                    info!("Удифферентовка судна на тихой воде не достигнута.");
-                }
-                info!("---------------Конец итерации {i}-------------\n");
-                i += 1;
-            }
-            if calc_disp < displacement {
-                min_draft_d = mean_draft;
-                mean_draft = (max_draft_d + min_draft_d) / 2.0;
+            self.solution_information(i, aft_draft, nose_draft, lbp, calc_disp)?;
+            if (calc_disp - displacement).abs() <= 0.004 * displacement {
+                return Ok((aft_draft, nose_draft));
             } else {
-                max_draft_d = mean_draft;
-                mean_draft = (max_draft_d + min_draft_d) / 2.0;
+                if calc_disp < displacement {
+                    min_draft_d = mean_draft;
+                    mean_draft = (max_draft_d + min_draft_d) / 2.0;
+                } else {
+                    max_draft_d = mean_draft;
+                    mean_draft = (max_draft_d + min_draft_d) / 2.0;
+                }
             }
         }
         Err(
