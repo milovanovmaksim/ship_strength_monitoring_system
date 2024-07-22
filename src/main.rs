ff19e6f4af727c8eca656497dfc4eb7743776c3e
milovanovmaksim <milovanov160386@gmail.com>
2024-07-22 20:21:42 +0300

    cnahge main func

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,8 @@ fn main() {
     env::set_var("RUST_LOG", "debug");
     env::set_var("RUST_BACKTRACE", "full");
     env_logger::init();
-    let strength = Strength::new_project();
+    let input_path = "./input_data/input_data.json".to_string();
+    let shiploads_file = "input_data/empty_ship.json".to_string();
+    let frames_file = "./input_data/frames.json".to_string();
+    let strength = Strength::new_project(input_path, shiploads_file, frames_file);
 }
