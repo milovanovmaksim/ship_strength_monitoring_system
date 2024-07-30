a1c427770296a565484dc90b7bd8ebbbd1a9fe7e
milovanovmaksim <milovanov160386@gmail.com>
2024-07-30 10:49:25 +0300

    correct a test

--- src/tests/unit/strength/deadweight/deadweight_intensity_test.rs
+++ src/tests/unit/strength/deadweight/deadweight_intensity_test.rs
@@ -33,7 +33,7 @@ mod tests {
             Shipload::new(5.0, Point::new(-64.0, 0.0, 0.0), 1.0),
         ]));
         let ship_dimensions = ShipDimensions::new(125.0, 20, 0.6);
-        let mut deadweight_intensity = DeadweightIntensity::new(shiploads, ship_dimensions);
+        let deadweight_intensity = DeadweightIntensity::new(shiploads, ship_dimensions);
         let test_spatium_functions = SpatiumFunctions::new(vec![
             SpatiumFunction::new(0, -62.5, -56.25, 1.39, 1.39),
             SpatiumFunction::new(1, -56.25, -50.0, -0.59, -0.59),
@@ -57,6 +57,6 @@ mod tests {
             SpatiumFunction::new(19, 56.25, 62.5, 1.39, 1.39),
         ]);
         let spatium_functions = deadweight_intensity.deadweight_intensity();
-        assert_eq!(&test_spatium_functions, spatium_functions);
+        assert_eq!(test_spatium_functions, spatium_functions);
     }
 }
